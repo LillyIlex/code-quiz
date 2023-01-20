6fb0347abc15bbeb588e8625ff18f961909d95de
LillyIlex <116085080+LillyIlex@users.noreply.github.com>
2023-01-20 13:58:38 +0000

    added first question and answers to appear in the startGame function

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,16 +1,105 @@
 // vars
 
+//questions
+
+// target html with query selectors
+var questionBox = document.querySelector("#question-box")
+var question = document.querySelector("#question-title")
+var answers = document.querySelector("#choices")
+var answerBtn = document.querySelector(".answer-btn") 
+var answer1 = document.getElementById("answer1")
+var answer2 = document.getElementById("answer2")
+var answer3 = document.getElementById("answer3")
+var answer4 = document.getElementById("answer4")
+//array of objects
+var questions = [
+    {
+        question: 'What does HTML stand for?',
+        answers: [
+            { answerBtn: 'Hyper Text Markup Link', correct: false },
+            { answerBtn: 'Hyper Text Markup Lion', correct: false },
+            { answerBtn: 'Hyper Text Markup Language', correct: true },
+            { answerBtn: 'Hyper Text Markup Losers', correct: false },
+        ]
+    },
+    {
+        question: 'Where do we create our repo`s?',
+        answers: [
+            { answerBtn: 'GitPub', correct: false },
+            { answerBtn: 'GitHun', correct: false },
+            { answerBtn: 'GitHub', correct: true },
+            { answerBtn: 'Gitify', correct: false },
+        ]
+    },
+    {
+
+        question: 'Where in the HTML would you insert the JavaScript link?',
+        answers: [
+            { answerBtn: 'Head', correct: false },
+            { answerBtn: 'Body', correct: true },
+            { answerBtn: 'Header', correct: false },
+            { answerBtn: 'Footer', correct: false },
+        ]
+    },
+    {
+        question: 'A `title` in a HTML doc...',
+        answers: [
+            { answerBtn: 'Names the page and displays in the web browser tab', correct: true },
+            { answerBtn: 'Gives your HTML file a new name locally', correct: false },
+            { answerBtn: 'Creates a visible heading on your web app', correct: false },
+            { answerBtn: 'Doesn`t exist', correct: false },
+        ]
+    }, {
+
+        question: 'Which is the correct function to get a radomised number?',
+        answers: [
+            { answerBtn: 'Math.floor()', correct: false },
+            { answerBtn: 'math.rand', correct: false },
+            { answerBtn: 'Math.ceil()', correct: false },
+            { answerBtn: 'Math.random()', correct: true },
+        ],
+    }, {
+
+        question: 'What do we use CSS for?',
+        answers: [
+            { answerBtn: 'To add styling', correct: true },
+            { answerBtn: 'To add text', correct: false },
+            { answerBtn: 'To add forms', correct: false },
+            { answerBtn: 'To add games', correct: false }
+        ],
+    }, {
+
+        question: '`var`, `const` and `let` are all what?',
+        answers: [
+            { answerBtn: 'Methods', correct: false },
+            { answerBtn: 'Functions', correct: false },
+            { answerBtn: 'Objects', correct: false },
+            { answerBtn: 'Variables', correct: true }
+        ],
+    }, {
+
+        question: 'What best describes how the DOM sees a web-page?',
+        answers: [
+            { answerBtn: 'Branch', correct: false },
+            { answerBtn: 'Tree', correct: true },
+            { answerBtn: 'Flower', correct: false },
+            { answerBtn: 'Bush', correct: false }
+        ]
+    }
+]
+
+
 var timer = document.querySelector(".timer")
 var timeEl = document.querySelector("#time")
 var startButton = document.getElementById("start")
 var submitForm = document.querySelector("#submit")
 var feedback = document.querySelector("#feedback")
-var answerButtons = document.querySelector("#choices")
-var questionBox= document.getElementById("question-box")
-var questionEl = document.getElementById("question-title")
-var questions 
 
-var shuffleQuestions, currentQuestionIndex
+var answerChoices = document.querySelector("#choices")
+var questionBox= document.getElementById("question-box")
+var questionTitle = document.getElementById("question-title")
+var questions 
+var startScreen = document.getElementById("start-screen") 
 
 var timerCount = 90;
 var winCounter = 0;
@@ -18,62 +107,78 @@ var isWin
 var isLose
 var finalScore
 
-
 //init on page load
 /*function init() {
     getWins();
     getlosses();
   } */
 
+  /*question1
+  if(correct === true) {
+    getQuestion2()
+  } else {
+*/
+ 
 //A start button that when clicked a timer starts and the first question appears.
 //addEventListener("click", startGame)
 
 function startGame() {
+  startScreen.classList.add("hide");
+  questionBox.classList.remove("hide");
+  //text content questionTitle.innerText()
+  console.log(questions[0].question)
+  //for ( var i = 0; i < questions.length; i++) {
+   //show 1 question at a time on web page
+   questionTitle.innerText = questions[0].question;
+   console.log(questions[0].answers)
+//target different answers
+   answer1.innerHTML = questions[0].answers[0].answerBtn;
+   answer2.innerHTML = questions[0].answers[1].answerBtn;
+   answer3.innerHTML = questions[0].answers[2].answerBtn;
+   answer4.innerHTML = questions[0].answers[3].answerBtn;
+   console.log(questions[0].answers[2].correct)
+   //click event 
+   answerBtn.addEventListener("click", function(){
+      if (questions[0].answers[2]) {
+    console.log("Correct!");
+   }
+   })
+ 
+  }
+
+  
 startButton.addEventListener("click", startGame);
-startButton.classList.add("hide");
-//shuffleQuestions = question.sort(() => Math.random() - .5)
-currentQuestionIndex = 0
-questionBox.classList.remove("hide");
-setNextQuestion()
-}
-startGame()
 
-function setNextQuestion() {
- // resetState()
-showQuestion()
-}
 
-function showQuestion(question) {
-  questionEl.innerText = question.question;
-  question.answers.forEach(answer => {
-    var button = document.createElement("button");
-    button.innerText = answer.text
-    button.classList.add("btn")
-    if (answer.correct) {
+function question2() {
+  //for ( var i = 0; i < questions.length; i++) {
+    //show 1 question at a time on web page
+    questionTitle.innerText = questions[1].question;
+    console.log(questions[0].answers[i])
+ //target different answers
+    answer1.innerHTML = questions[1].answers[0].answerBtn;
+    answer2.innerHTML = questions[1].answers[1].answerBtn;
+    answer3.innerHTML = questions[1].answers[2].answerBtn;
+    answer4.innerHTML = questions[1].answers[3].answerBtn;
+    if (questions[0].answers[i].correct === true) {
+    }
+   }
+
+/*   if (answer.correct) {
       button.dataset.correct = answer.correct
     }
     button.addEventListener("click", selectAnswer)
-  })
-}
-function selectAnswer() {
+  }) */
 
-}
 
-/* function resetState() {
-  nextButton.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonElement.firstChild)
-  }
-} */
-
-function selectAnswer (event) {
+/* function selectAnswer (event) {
   var selectedButton = event.target
   var correct = selectedButton.dataset.correct;
   setStatusClass(document.body, correct)
-  Array.from(answerBUttonElement.children).forEach(button => {
+  Array.from(answerButtonElement.children).forEach(button => {
     setStatusClass(button, button.dataset,correct)
   })
-}
+} */
 
 function setStatusClass (element, correct) {
   clearStatusClass(element)
@@ -119,7 +224,7 @@ function startTimer() {
         loseGame();
       }
     }, 1000);
-  }
+  } 
 
   //-10 seconds for wrong answers
 
